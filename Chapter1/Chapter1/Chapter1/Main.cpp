3537a48923aa3f654faef3ef90dbe9774002a076
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-03 17:13:28 +0300

    lonhCout

--- Chapter1/Chapter1/Chapter1/Main.cpp
+++ Chapter1/Chapter1/Chapter1/Main.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main() {
 	
 	int firstNumber = 0, secondNumber = 0;
-	cout << "Input number: ";
-	cin >> firstNumber >> secondNumber;
-	cout << "Sum=" << firstNumber + secondNumber<<endl;
+	
+	cout << "The sum of" << firstNumber
+		<< "and" << secondNumber
+	<< "is" << firstNumber + secondNumber << endl;
+
 	system("pause");
 	return 0;
 }
