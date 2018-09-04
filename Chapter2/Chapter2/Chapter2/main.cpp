b2c7cf84849bfd443f422ebc11079e3c7c3982c9
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-04 21:30:57 +0300

    initialization

---
+++ Chapter2/Chapter2/Chapter2/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	//cin >> int inputValu;//error
+	//int i{ 3.14 };//error
+	int i = 3.14;
+
+	cout << i;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
