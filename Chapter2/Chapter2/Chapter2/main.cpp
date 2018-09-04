3d6ce52a332ba8ccaaa61ca39783e63a96218988
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-04 22:26:53 +0300

    reference

--- Chapter2/Chapter2/Chapter2/main.cpp
+++ Chapter2/Chapter2/Chapter2/main.cpp
@@ -5,12 +5,29 @@ using namespace std;
 
 int main() {
 
-	int i = 100, sum = 0;
+	//int &r = 10;//Error Not Literal
+	//int a = 10;
+	//int &r = a;//ok
+	//cout << r;//10
+	//cout << &r;//adres in memory
 
-	for (int i = 0; i != 10; i++)
-		sum += i;
+	//double a = 10.2;
+	//int &c = a;//Error double!=int
 
-	cout << "i=" << i << ", sum=" << sum;
+	//int a = 1.1;
+	//int &r = a;
+	//ok
+
+
+	//int &t = 1.1;//Error bad init 
+	//int &tr;//Error not Intit
+
+	int i, &ri = i;
+	i = 5;
+	ri = 10;
+	cout << i << " " << ri;
+
+
 	cout << endl;
 	system("pause");
 	return 0;
