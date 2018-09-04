48cdb2767106b6f63595328efc113ef036207e1a
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-04 22:55:01 +0300

    pointer

--- Chapter2/Chapter2/Chapter2/main.cpp
+++ Chapter2/Chapter2/Chapter2/main.cpp
@@ -4,29 +4,13 @@ using namespace std;
 
 
 int main() {
+	int i = 45;
+	int *p = &i;
+	*p = *p**p;
 
-	//int &r = 10;//Error Not Literal
-	//int a = 10;
-	//int &r = a;//ok
-	//cout << r;//10
-	//cout << &r;//adres in memory
-
-	//double a = 10.2;
-	//int &c = a;//Error double!=int
-
-	//int a = 1.1;
-	//int &r = a;
-	//ok
-
-
-	//int &t = 1.1;//Error bad init 
-	//int &tr;//Error not Intit
-
-	int i, &ri = i;
-	i = 5;
-	ri = 10;
-	cout << i << " " << ri;
-
+	cout << *p << endl;
+	cout << i << endl;
+	cout << 45 * 45;
 
 	cout << endl;
 	system("pause");
