0c19ce700dd03eaa8ee86666d47520f60849b824
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-05 18:45:02 +0300

    empty,size

--- Chapter3/Chapter3/Chapter3/main.cpp
+++ Chapter3/Chapter3/Chapter3/main.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main(){
-	string str;
+	string str="d";
 
-	while (getline(cin,str) ){
-		cout << str << endl;
+	if (str.empty()) {
+		getline(cin, str);
+		cout << "Text: << " << str << " >>" << endl;
+		cout << "Size text: < " << str.size() << " >"<<endl;
+	}
+	else
+	{
+		cout << "STR NOT EMPTY"<<endl;
 	}
 
 	system("pause");
