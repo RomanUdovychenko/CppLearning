90533b55bc672c4b6f56a7dc7e34932c4fd56e0f
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-05 18:10:35 +0300

    Chapter3
    String, vector, array

--- Chapter2/Chapter2/Chapter2/main.cpp
+++ Chapter2/Chapter2/Chapter2/main.cpp
@@ -14,10 +14,9 @@ int main() {
 	* cout << 45 * 45;
 	*/
 
-	int i = 0;
-	int *p = i;
+	//int i = 0;
+	//int *p = i;//Eroor can't initialization
 	//double *p = &i;//error int!=double
-
 
 	cout << endl;
 	system("pause");
