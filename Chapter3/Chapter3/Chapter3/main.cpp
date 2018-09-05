7fc60890c44fbd515b4a18263706f8b40bcea1a6
RomanUdovychenko <30699129+RomanUdovychenko@users.noreply.github.com>
2018-09-05 21:31:26 +0300

    Task2Strinng

--- Chapter3/Chapter3/Chapter3/main.cpp
+++ Chapter3/Chapter3/Chapter3/main.cpp
@@ -3,31 +3,13 @@
 using namespace std;
 
 int main(){
-	string str1,str2;
+	string strSum,strCin;
 	
-	getline(cin, str1);
-	getline(cin, str2);
-	//size
-	if (str1.size() == str2.size()) {
-		cout << "(size)str1 and str2 ==" << endl;
+	while (cin>>strCin)
+	{
+		strSum += strCin+" ";
+		cout << "New Str: " << strSum << endl;
 	}
-	else if (str1.size() > str2.size()) {
-		cout << "(size)str1>str2" << endl;
-	}
-	else {
-		cout << "(size)str1<str2" << endl;
-	}
-	//not size
-	if (str1 == str2) {
-		cout << "str1 and str2 ==" << endl;
-	}
-	else if (str1 > str2) {
-		cout << "str1>str2" << endl;
-	}
-	else {
-		cout << "str1<str2" << endl;
-	}
-
 
 	system("pause");
 	return 0;
